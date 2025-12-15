6a0fd9502e78a6c892bfed4d664220d7de653674
Countgreen <jsrharshverma@gmail.com>
2025-12-15 13:31:44 +0530

    uvicorn integrated

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
+import dagshub
+dagshub.init(repo_owner='Countgreen', repo_name='Network_Security_System', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
